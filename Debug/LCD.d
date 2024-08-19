dc81eca4dc4e6f6fc2be23e002f48d22f8091d4a
nadiahelal175 <nadiahelal@gmail.com>
2024-08-19 18:43:09 +0300

    first commit

---
+++ Debug/LCD.d
@@ -0,0 +1,25 @@
+LCD.d LCD.o: .././LCD.c .././LCD.h .././DIO.h .././LCD_config.h \
+ d:\program\ files\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/util/delay.h \
+ d:\program\ files\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/inttypes.h \
+ d:\program\ files\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/include/stdint.h \
+ d:\program\ files\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/stdint.h \
+ d:\program\ files\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/util/delay_basic.h \
+ d:\program\ files\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/math.h
+
+.././LCD.h:
+
+.././DIO.h:
+
+.././LCD_config.h:
+
+d:\program\ files\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/util/delay.h:
+
+d:\program\ files\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/inttypes.h:
+
+d:\program\ files\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/include/stdint.h:
+
+d:\program\ files\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/stdint.h:
+
+d:\program\ files\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/util/delay_basic.h:
+
+d:\program\ files\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/math.h:
---
+++ button.h
@@ -0,0 +1,6 @@
+
+#ifndef BUTTON_H_
+#define BUTTON_H_
+void button_vInit(unsigned char portname,unsigned char pinnumber);
+unsigned char button_u8read(unsigned char portname,unsigned char pinnumber);
+#endif /* BUTTON_H_ */
